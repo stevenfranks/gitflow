6e41720a9d600b9a8efb2de8f9acb974adc93e74
Steven Franks <steven.franks@british-gymnastics.org>
2017-12-18 15:46:20 +0000

    Add: Improved CLI with the use of prompt_toolkit. Still WIP

--- gitflow.py
+++ gitflow.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import unicode_literals # for python 2 - convert all strings to unicode
+
 import re
 import os
 import sys
@@ -7,8 +9,32 @@ import getopt
 import os.path
 import subprocess
 
-# print 'Number of arguments:', len(sys.argv), 'arguments.'
-# print 'Argument List:', str(sys.argv)
+from prompt_toolkit import prompt
+from prompt_toolkit.contrib.completers import WordCompleter
+
+from prompt_toolkit.token import Token
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.styles import style_from_dict
+
+import getpass
+
+example_style = style_from_dict({
+    # User input.
+    Token:          '#ff0066',
+
+    # Prompt.
+    Token.Username: '#884444',
+    Token.At:       '#00aa00',
+    Token.Colon:    '#00aa00',
+    Token.Pound:    '#00aa00',
+    Token.Host:     '#000088 bg:#aaaaff',
+    Token.Path:     '#884444 underline',
+    Token.Toolbar: '#ffffff bg:#333333',
+})
+
+syntax_completer = WordCompleter(['start', 'finish', 'hotfix', 'help', 'exit', 'check'])
+# text = prompt(get_prompt_tokens=get_prompt_tokens, style=example_style, completer=syntax_completer)
+
 
 version_file = '.version'
 lastNum = re.compile(r'(?:[^\d]*(\d+)[^\d]*)+')
@@ -36,23 +62,24 @@ def get_current_version(version_file):
 
             # get current version
             first_line = f.readline().strip()
-            print 'Current version is %s..' % first_line
+            # print 'Current version is %s..' % first_line
 
             current = first_line
+            # current = 'Current version is %s ' % first_line
 
             return current
     else:
 
-        print 'Version file not found.. Exiting'
-        sys.exit()
+        print 'Version file not found'
+
 
 def hotfix():
 
     print 'Using mode: hotfix'
 
-    print '----------'
-    print 'Updating branches'
-    print '----------'
+    # print '----------'
+    # print 'Updating branches'
+    # print '----------'
 
     # make sure we have the latest changes
     os.system('git checkout master; git pull origin master;')
@@ -65,8 +92,11 @@ def hotfix():
     # increment version number
     new_version = increment(current_version)
 
-    print 'Do you want to bump to %s? (y/n)' %new_version
-    confirm_new_version = raw_input(':')
+    # print 'Do you want to bump to %s? (y/n)' %new_version
+    # confirm_new_version = raw_input(':')
+
+    confirm_new_version = prompt('Do you want to bump to %s? (y/n) > ' %new_version, completer=syntax_completer, style=example_style, )
+
 
     if confirm_new_version == 'y':
 
@@ -119,6 +149,94 @@ def finish_hotfix():
         print 'Finished hotfix.. Exiting'
         print 'Don\'t forget to tag!'
         print '----------'
+
+
+def check_branch():
+    branch = os.system('git show-branch remotes/origin/master')
+    return branch
+
+
+def get_bottom_toolbar_tokens(cli):
+    # get current version
+    current_version = get_current_version(version_file)
+    return [(Token.Toolbar, current_version)]
+
+
+logo = """
+      :::::::::   :::::::: 
+     :+:    :+: :+:    :+: 
+    +:+    +:+ +:+         
+   +#++:++#+  :#:          
+  +#+    +#+ +#+   +#+#    
+ #+#    #+# #+#    #+#     
+#########   ########       
+
+Type a command and press enter:
+"""
+
+
+logo = """
+        .__  __    _____.__                 
+   ____ |__|/  |__/ ____\  |   ______  _  ___
+  / ___\|  \   __\   __\|  |  /  _ \ \/ \ / /
+ / /_/  >  ||  |  |  |  |  |_(  <_> )      / 
+ \___  /|__||__|  |__|  |____/\____/ \_/\_/  
+/_____/  v1.0.0                                   
+                                                
+Type a command and press enter:
+"""
+
+
+# start gitflow cli
+
+os.system('clear')
+
+try:
+
+    text = prompt(logo + '> ', style=example_style, completer=syntax_completer, vi_mode=True)
+
+except EOFError:
+
+    print 'Bye!'
+    sys.exit()
+
+except KeyboardInterrupt:
+
+    print 'Bye!'
+    sys.exit()
+
+
+# keep prompt running
+while True:
+
+    try:
+
+        if text.strip() == 'exit':
+            break
+
+        elif text.strip() == 'help':
+            print 'Check if current branch exists: check'
+            print 'Enter hotfix mode: hotfix'
+            print 'Exit gitflow: exit'
+            print 'Show current logfile: show log'
+
+        elif text.strip() == 'hotfix' or text.strip() == 'start':
+            hotfix()
+
+        elif text.strip() == 'check':
+            check_branch()
+
+    except EOFError:
+
+        print 'Bye!'
+
+    except:
+
+        pass
+
+    text = prompt('> ', completer=syntax_completer, style=example_style, )
+    # text = prompt('> ', completer=syntax_completer, get_bottom_toolbar_tokens=get_bottom_toolbar_tokens, style=example_style, )
+
 
 def main(argv):
 
